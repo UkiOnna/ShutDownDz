6f710c449ab4c54e036bf11c894d4f22c780083f
UkiOnna <sikirovtemirlan@mail.ru>
2018-06-06 17:54:28 +0600

    hop

---
+++ ClientShutUp/ClientShutUp/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClientShutUp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            UdpClient reciever = new UdpClient(161);
+            reciever.JoinMulticastGroup(IPAddress.Parse("224.0.0.0"), 50);
+            IPEndPoint endPoint = null;
+            try
+            {
+                while (true)
+                {
+                    byte[] data = reciever.Receive(ref endPoint);
+                    if (Encoding.UTF8.GetString(data)=="t")
+                    {
+                        Console.WriteLine("sd");
+                    }
+                }
+            }
+            catch(SocketException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            finally
+            {
+                reciever.DropMulticastGroup(IPAddress.Parse("224.0.0.0"));
+                reciever.Close();
+            }
+        }
+    }
+}
---
+++ ServerShutUp/ServerShutUp/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ServerShutUp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            UdpClient sender = new UdpClient();
+            sender.JoinMulticastGroup(IPAddress.Parse("224.0.0.0"), 50);
+
+
+            IPAddress remoteAddress = IPAddress.Parse("224.0.0.0");
+            IPEndPoint endPoint = new IPEndPoint(remoteAddress, 161);
+
+            while (true)
+            {
+                Console.WriteLine("Для выключения введите 1");
+                string command = Console.ReadLine();
+                if (command == "1")
+                {
+                    byte[] data = Encoding.Default.GetBytes("t");
+                    sender.Send(data, data.Length, endPoint);
+
+                }
+            }
+            
+        }
+    }
+}
